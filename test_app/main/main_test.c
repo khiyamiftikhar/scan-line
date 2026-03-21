e906d50ee01990500a973b9b7bd1a6e99f745a1e
khiyam <khiyamiftikhar@gmail.com>
2026-03-21 15:24:59 +0500

    feat: added

---
+++ test_app/main/main_test.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include "unity.h"
+#include "esp_err.h"
+
+
+
+void app_main(){
+
+
+  UNITY_BEGIN();
+  /*
+  RUN_TEST(test_exact_pulse_widths_are_identified_correctly);
+  RUN_TEST(test_pulse_within_tolerance_boundary_is_accepted);
+  RUN_TEST(test_out_of_range_pulse_fires_no_callback);
+  RUN_TEST(test_pulse_below_filter_floor_is_rejected);
+  RUN_TEST(test_pulse_above_filter_ceiling_is_rejected);
+  RUN_TEST(test_multiple_sequential_pulses_all_delivered);
+  UNITY_END();
+
+//  unity_run_all_tests();
+*/
+
+  while(1){
+
+    
+    
+    
+     
+   unity_run_menu();
+
+
+  }
+   
+
+
+}
